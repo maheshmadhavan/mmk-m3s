8e4692d1f3b013561e35d3d48834ad46fd0ec1ac
Subina K K <skk@premiergp.com>
2017-05-03 11:57:20 +0530

    MTP-5 Forgot Password Page Signed-off-by: Subina K K <skk@premiergp.com>

--- src/app/pages/forgot-password.component.ts
+++ src/app/pages/forgot-password.component.ts
@@ -3,15 +3,29 @@ import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: 'forgot-password.component.html',
-  styleUrls :['../app.component.css']
+  styleUrls: ['../../assets/css/custom.css']
 })
 export class ForgotPasswordComponent {
   @ViewChild('f') forgotForm: NgForm;
   success = false;
+  notValid;
 	constructor() { }
   
-  onResetPass() {     
-   this.success = true; 
-   this.forgotForm.reset();   
+  onResetPass() {
+    this.notValid = "";
+    if(this.forgotForm.valid){
+      if(this.forgotForm.value.email === 'demo@premiergp.com'){
+        this.success = true; 
+        this.forgotForm.reset(); 
+      }
+      else{
+        this.notValid ="The email id does not exists"; 
+      }
+    }      
+    else{
+      this.notValid ="Please enter a valid email!"; 
+    } 
+  
+     
   }
 }

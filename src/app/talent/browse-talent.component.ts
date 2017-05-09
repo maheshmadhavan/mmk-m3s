65d391b7d585e974d626386747c57b312738dbda
Lakshmikanth V Mallar <lvm@premiergp.com>
2017-05-09 12:55:44 +0530

    Added Click-able Star in filter.
    Signed-off-by: Lakshmikanth V Mallar <lvm@premiergp.com>

--- src/app/talent/browse-talent.component.ts
+++ src/app/talent/browse-talent.component.ts
@@ -5,11 +5,13 @@ import { BrowseTalent } from '../models/browse-talent.model';
 import { BrowseTalentService } from '../services/browse-talent.service'; 
 
 @Component({
-  templateUrl: './browse-talent.component.html'
+  templateUrl: './browse-talent.component.html',
+  styleUrls : ['./browse-talent.component.css']
 })
 export class BrowseTalentComponent implements OnInit {
   browsetalents: BrowseTalent[] = [];
   @ViewChild('filterModal') public filterModal:ElementRef;
+  selectedStarIndex = 0;
 
   constructor(private browseTalentService: BrowseTalentService) { }
 
@@ -29,5 +31,18 @@ export class BrowseTalentComponent implements OnInit {
       a.push(i+1) 
     } 
     return a; 
+  }
+
+  clickableStarRange(value: number) {
+    let a = []; 
+    for(let i = value; i > 0; i--) { 
+      a.push(i) 
+    } 
+    return a; 
+  }
+
+  selectedStar(starIndex: number) {
+    this.selectedStarIndex = starIndex;
+    console.log(this.selectedStarIndex);
   }
 }
